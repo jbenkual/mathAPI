4468f10fe41bfee1bfefb8ca6df4ea4409eacffc
jbenkual <joe@benkual.com>
2015-09-08 19:57:40 -0700

    Add a switch statement for math operations
    The server parses the input and uses the switch statement to evaluate the expression.
    The end result is sent back as the body of the response.

--- server.js
+++ server.js
@@ -4,7 +4,28 @@ var PORT = 8080;
 
 var server = http.createServer(function(req, res) {
 
-	console.log(req.url);
+	var data = req.url.split('/');
+	var op = data[2];
+
+	var total = data.splice(3).reduce(function(total, num){
+		num = parseInt(num);
+		switch (op) {
+			case 'add':
+				return total + num;
+			case 'subtract':
+				return total - num;
+			case 'multiply':
+				return total * num;
+			case 'divide':
+				return total / num;
+			default:
+				return num;
+		}
+	}, 0);
+
+	res.write(total + "");
+
+	res.end();
 
 });
 
